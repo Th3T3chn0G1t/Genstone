3ed04b08d6aeac1514efe37380fb0977e260c19c
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2021-07-24 10:36:51 +0100

    Fixed some building issues with genesis API inclusion Fixed a macOS platform error in threads_posix.h

--- sandbox/sample/main.c
+++ sandbox/sample/main.c
@@ -1,5 +1,17 @@
-#include <stdio.h>
+#include <gencommon.h>
+
+int thread_func(void* pass) {
+    printf("Hello, C11 threads!\n");
+
+    return 0;
+}
 
 int main() {
     printf("Hello, Genesis!\n");
+
+    thrd_t thread;
+    thrd_create(&thread, thread_func, NULL);
+
+    int result;
+    thrd_join(thread, &result);
 }
