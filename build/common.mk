21905da6efea2a3107c61a665c67ea1950cf2b1c
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2021-07-25 22:31:23 +0100

    Trying things the long way

--- build/common.mk
+++ build/common.mk
@@ -35,7 +35,8 @@ ifeq ($(PLATFORM),WIN)
 	DYNAMIC_LIB_C_FLAGS =
 	DYNAMIC_LIB_L_FLAGS =
 
-	DYNAMIC_LIB_TOOL = dlltool --export-all-symbols -l $(subst $(DYNAMIC_LIB_SUFFIX),$(STATIC_LIB_SUFFIX),$@) -D $(notdir $@) $(filter %.o,$^); $(LINKER) -shared -Wl,-implib:$(subst $(DYNAMIC_LIB_SUFFIX),$(STATIC_LIB_SUFFIX),$@) -o $@ $(filter %.o,$^)
+#	DYNAMIC_LIB_TOOL = dlltool --export-all-symbols -l $(subst $(DYNAMIC_LIB_SUFFIX),$(STATIC_LIB_SUFFIX),$@) -D $(notdir $@) $(filter %.o,$^); $(LINKER) -shared -Wl,-implib:$(subst $(DYNAMIC_LIB_SUFFIX),$(STATIC_LIB_SUFFIX),$@) -o $@ $(filter %.o,$^)
+	DYNAMIC_LIB_TOOL = dlltool --export-all-symbols -z $(subst $(DYNAMIC_LIB_SUFFIX),.def,$@) -e $(subst $(DYNAMIC_LIB_SUFFIX),.o,$@) -l $(subst $(DYNAMIC_LIB_SUFFIX),$(STATIC_LIB_SUFFIX),$@) -D $(notdir $@) $(filter %.o,$^); $(LINKER) -shared -o $@ $(filter %.o,$^) $(subst $(DYNAMIC_LIB_SUFFIX),.o,$@) -Wl,-implib:$(subst $(DYNAMIC_LIB_SUFFIX),$(STATIC_LIB_SUFFIX),$@),-def:$(subst $(DYNAMIC_LIB_SUFFIX),.def,$@)
 	STATIC_LIB_TOOL = ar -cvq $@ $(filter %.o,$^)
 endif
 ifeq ($(PLATFORM),LNX)
