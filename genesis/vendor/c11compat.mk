9c783ad14ec6e8fb9510f2e21bcecdb3a4d0490d
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2021-07-25 21:55:22 +0100

    Windows doesn;t need uchar emulation

--- genesis/vendor/c11compat.mk
+++ genesis/vendor/c11compat.mk
@@ -1,10 +1,10 @@
-C11COMPAT = 0 # Windows & macOS always need, Linux never needs
-C11COMPAT_UCHAR = 0 # BSD never needs
+C11COMPAT = 0 # macOS always needs, Linux never needs
+C11COMPAT_UCHAR = 0 # BSD & Windows never need
 C11COMPAT_THREADS = 0 # FreeBSD never needs but other flavours do
 
 # 		UCHAR		THREADS
 # LNX	0			0
-# WIN	1			1
+# WIN	0			1
 # DWN	1			1
 # BSD	0			FreeBSD 0, Other 1
 
@@ -18,7 +18,7 @@ ifeq ($(PLATFORM),LNX)
 endif
 ifeq ($(PLATFORM),WIN)
 	C11COMPAT = 1
-	C11COMPAT_UCHAR = 1
+	C11COMPAT_UCHAR = 0
 	C11COMPAT_THREADS = 1
 
 	C11COMPAT_CFLAGS =
