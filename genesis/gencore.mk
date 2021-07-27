39f3fe42837c4c3d04c87cebc6bb17a501b46548
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2021-07-28 00:27:51 +0100

    Fixed dirent not being included internally for windows

--- genesis/gencore.mk
+++ genesis/gencore.mk
@@ -11,7 +11,11 @@ GEN_CORE_LIB = lib/$(LIB_PREFIX)gencore$(DYNAMIC_LIB_SUFFIX)
 
 gencore: $(GEN_CORE_LIB)
 
-$(GEN_CORE_LIB): CFLAGS = $(C11_COMPAT_CFLAGS)
+ifeq ($(PLATFORM),WIN)
+_GEN_CORE_CFLAGS += -Igenesis/vendor/dirent/include # This is kind of annoying but whatever
+endif
+
+$(GEN_CORE_LIB): CFLAGS = $(C11_COMPAT_CFLAGS) $(_GEN_CORE_CFLAGS)
 $(GEN_CORE_LIB): LFLAGS = -Llib $(C11_COMPAT_LFLAGS)
 $(GEN_CORE_LIB): $(GEN_CORE_OBJECTS) $(C11_COMPAT_LIB) | lib
 
