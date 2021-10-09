42270b1cebc0dbee3aa11ad7e237c6cea98dfe63
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2021-10-09 19:55:26 +0100

    Trying to dynamically locate the dumpbin exec

--- script/winimplibgen.bat
+++ script/winimplibgen.bat
@@ -1,9 +1,20 @@
 rem https://stackoverflow.com/questions/9946322/how-to-generate-an-import-library-lib-file-from-a-dll/39916789#39916789
 
 @echo off
-
 setlocal enabledelayedexpansion
-for /f "tokens=1-4" %%1 in ('dumpbin /exports %1') do (
+
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+
+if exist "%InstallDir%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt" (
+  set /p Version=<"%InstallDir%\VC\Auxiliary\Build\Microsoft.VCToolsVersion.default.txt"
+  set Version=!Version: =!
+)
+
+rem "%InstallDir%\VC\Tools\MSVC\%Version%\bin\HostX64\x64\dumpbin.exe"
+rem for /f "tokens=1-4" %%1 in ('dumpbin /exports %1') do (
+for /f "tokens=1-4" %%1 in ('"%InstallDir%\VC\Tools\MSVC\%Version%\bin\HostX64\x64\dumpbin.exe" /exports %1') do (
     set /a ordinal=%%1 2>nul
     set /a hint=0x%%2 2>nul
     set /a rva=0x%%3 2>nul
