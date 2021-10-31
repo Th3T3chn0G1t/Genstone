e48e11c277aae237b78a8b51812df59c716c4ed5
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2021-10-31 20:49:52 +0000

    Missed changed from last commit

--- sandbox/sample.mk
+++ sandbox/sample.mk
@@ -3,16 +3,19 @@ SAMPLE_OBJECTS = $(SAMPLE_SOURCES:.c=.o)
 
 SAMPLE_EXEC = sample$(EXECUTABLE_SUFFIX)
 
+.ONESHELL:
 build_message_sample:
 	@echo "$(SECTION_PREFIX) Sample Project"
 	@echo "$(INFO_PREFIX) Genstone sandbox sample project!"
 
+.ONESHELL:
 sample: gencore $(SAMPLE_EXEC) ### @User Builds a sample sandbox project
 
 $(SAMPLE_EXEC): CFLAGS = $(GEN_CORE_CFLAGS)
 $(SAMPLE_EXEC): LFLAGS = -Llib $(GEN_CORE_LFLAGS)
 $(SAMPLE_EXEC): $(SAMPLE_OBJECTS)
 
+.ONESHELL:
 clean_sample:
 	-$(RM) $(subst /,$(SEP),$(SAMPLE_OBJECTS))
 	-$(RM) $(subst /,$(SEP),$(SAMPLE_EXEC))
