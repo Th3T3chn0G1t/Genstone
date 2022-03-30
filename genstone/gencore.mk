d95a15e65a4b2a9a692cbeac36f2955116d0d6ff
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2022-03-30 14:01:31 +0100

    Added -W flag to allow GNU `?:` in gencore

--- genstone/gencore.mk
+++ genstone/gencore.mk
@@ -1,4 +1,4 @@
-GEN_CORE_DIAGNOSTIC_FLAGS = -Werror -Weverything -Wno-gnu-statement-expression -Wno-c++98-compat -Wno-redundant-parens -Wno-atomic-implicit-seq-cst -Wno-padded -Wno-poison-system-directories -Wno-unknown-warning-option  -Wno-c++98-compat-pedantic -Wno-old-style-cast -Wno-overlength-strings -Wno-cast-qual
+GEN_CORE_DIAGNOSTIC_FLAGS = -Werror -Weverything -Wno-gnu-statement-expression -Wno-c++98-compat -Wno-redundant-parens -Wno-atomic-implicit-seq-cst -Wno-padded -Wno-poison-system-directories -Wno-unknown-warning-option  -Wno-c++98-compat-pedantic -Wno-old-style-cast -Wno-overlength-strings -Wno-cast-qual -Wno-gnu-conditional-omitted-operand
 # https://stackoverflow.com/questions/28516413/c11-alignas-vs-clang-wcast-align
 # Clang fails to check alignment properly when delivering -Wcast-align
 GEN_CORE_DIAGNOSTIC_FLAGS += -Wno-cast-align
