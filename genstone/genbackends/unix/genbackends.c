12f45f05b3a618a2e189950aad3bb7c15ce4aafd
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2022-11-24 12:11:55 +0000

    Added fallback from BSD-like specific `RTLD_SELF`

--- genstone/genbackends/unix/genbackends.c
+++ genstone/genbackends/unix/genbackends.c
@@ -7,14 +7,17 @@
 GEN_PRAGMA(GEN_PRAGMA_DIAGNOSTIC_REGION_BEGIN)
 GEN_PRAGMA(GEN_PRAGMA_DIAGNOSTIC_REGION_IGNORE("-Weverything"))
 #include <dlfcn.h>
-// #include <stdio.h>
 GEN_PRAGMA(GEN_PRAGMA_DIAGNOSTIC_REGION_END)
 
+#ifdef RTLD_SELF // BSD-ish specific
+static void* gen_backends_internal_selfsym_handle = RTLD_SELF;
+#else
+static void* gen_backends_internal_selfsym_handle = RTLD_DEFAULT;
+#endif
+
 void* gen_backends_resolve_deferred(const char* const restrict symbol) {
-    // printf("Resolving `%s`...\n", symbol);
     void* ptr = dlsym(RTLD_SELF, symbol);
     char* error = dlerror();
     if(!error) return ptr;
-    // printf("Got error: `%s`\n", error);
     return GEN_NULL;
 }
